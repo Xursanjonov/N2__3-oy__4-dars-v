46d1985c01b7b6dc905591e2ec4b9609b676d23e
Nusratillo <nxursanjonov888@gmail.com>
2023-10-23 13:39:52 +0500

    JavaScript 1-dars vazifasi

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1-usul
+let interests = ['sport', 'cars', 'song', 'IT'];
+let interesting = ['football', 'basketball', 'Lamborghini sport car', 'C/C++', 'Python'];
+
+let child = {
+    fname: 'Komil',
+    lname: 'Temirov',
+    age: 14,
+    country: "AQSh",
+    state: "New-York",
+    district: "Chinatown",
+    street: "Bayard st",
+    school: "NEMT+m school",
+    hobby: interests,
+    what_They_Know: interesting,
+    favorite_food: ["lavash", 'hamburger']
+}
+
+// 2-usul
+
+/*
+let child = {
+    fullname: 'Komil Temirov',
+    age: 14,
+    country: "AQSh",
+    state: "New-York",
+    district: "Chinatown",
+    street: "Bayard st",
+    school: "NEMT+m school",
+    hobby: ['sport', 'cars', 'song', 'IT'],
+    what_They_Know: ['football', 'basketball', 'Lamborghini sport car', 'C/C++', 'Python'],
+    favorite_food: ["lavash", 'hamburger']
+}
+
+*/
+
+console.log(child)
